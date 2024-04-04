de66344808323ac1bc74847e58253290cab0e8a3
jasonfic <43254577+jasonfic@users.noreply.github.com>
2024-04-04 19:19:24 -0400

    Update User Schema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
-// Imports
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Define User model
 const UserSchema = new mongoose.Schema({
-    title: {type: String}
+  username: {type: String, required: true, unique: true },
+  password: {type: String, required: true},
+  username: {type: String, required: true, unique: true },
+  first_name: {type: String, required: true },
+  last_name: {type: String, required: true },
+  last_login: {type: Date, default: Date.now }
 });
 
 // Export model
