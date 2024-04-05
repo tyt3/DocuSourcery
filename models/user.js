3bc1cc6922ab32119e141c27b4996dba7a8b568e
Tyrica Terry Kapral <tyt3@pitt.edu>
2024-04-05 09:53:11 -0400

    Format document + Remove repeated attribute

--- models/user.js
+++ models/user.js
@@ -4,13 +4,12 @@ const Schema = mongoose.Schema;
 
 // Define User model
 const UserSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true },
-  password: {type: String, required: true},
-  username: {type: String, required: true, unique: true },
-  first_name: {type: String, required: true },
-  last_name: {type: String, required: true },
-  last_login: {type: Date, default: Date.now }
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  last_login: { type: Date, default: Date.now }
 });
 
 // Export model
-module.exports  = mongoose.model("user", UserSchema);
+module.exports = mongoose.model("user", UserSchema);
