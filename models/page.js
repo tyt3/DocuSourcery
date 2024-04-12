56c04a2936beece333c0fa628141c1bcdf18835b
Tyrica Terry Kapral <tyt3@pitt.edu>
2024-04-12 19:43:51 -0400

    Fix import

--- models/page.js
+++ models/page.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { populateCreatedBy: populateCurrentUser } = require('./middleware');
+const { populateCurrentUser: populateCurrentUser } = require('../routes/middleware');
 
 
 // Define User model
