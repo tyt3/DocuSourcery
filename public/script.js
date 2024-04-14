8a7350d67f6fd83aadd046ae12b56a80f1faf373
Tyrica Terry Kapral <tyt3@pitt.edu>
2024-04-13 21:54:59 -0400

    Comment out code

--- public/script.js
+++ public/script.js
@@ -1,53 +1,53 @@
-//  Implement AJAX requests to server-side
+// //  Implement AJAX requests to server-side
 
-// Create an XMLHttpRequest Object
-var xhr = new XMLHttpRequest();
+// // Create an XMLHttpRequest Object
+// var xhr = new XMLHttpRequest();
 
-// Configure the Request
-xhr.open("GET", "The URL comes here", true);
+// // Configure the Request
+// xhr.open("GET", "The URL comes here", true);
 
-//Send the Request
-xhr.send();
+// //Send the Request
+// xhr.send();
 
-// Handle the Response
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        console.log(xhr.responseText);
-    }
-};
+// // Handle the Response
+// xhr.onload = function() {
+//     if (xhr.status === 200) {
+//         console.log(xhr.responseText);
+//     }
+// };
 
-// Displaying Data
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        document.getElementById("result").innerHTML = data.message;
-    }
-};
+// // Displaying Data
+// xhr.onload = function() {
+//     if (xhr.status === 200) {
+//         var data = JSON.parse(xhr.responseText);
+//         document.getElementById("result").innerHTML = data.message;
+//     }
+// };
 
-// Creating a POST request with AJAX
+// // Creating a POST request with AJAX
 
-// Create XMLHttpRequest
-var xhr = new XMLHttpRequest();
+// // Create XMLHttpRequest
+// var xhr = new XMLHttpRequest();
 
-//  Configure Request
-xhr.open("POST", "https://api.example.com/endpoint", true);
+// //  Configure Request
+// xhr.open("POST", "https://api.example.com/endpoint", true);
 
-// Send Data
-var data = {
-    key1: 'value1',
-    key2: 'value2'
-};
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify(data));
+// // Send Data
+// var data = {
+//     key1: 'value1',
+//     key2: 'value2'
+// };
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.send(JSON.stringify(data));
 
-// Handle Response
+// // Handle Response
 
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        // Handle the response data
-    }
-};
+// xhr.onload = function() {
+//     if (xhr.status === 200) {
+//         var response = JSON.parse(xhr.responseText);
+//         // Handle the response data
+//     }
+// };
 
 // Make action-button div dynamically sticky 
 const actionButtons = document.getElementById('action-buttons');
