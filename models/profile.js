dffbe7b9d763be6f56e72f01aea1ce0b416f88b2
jasonfic <jasonficorilli@gmail.com>
2024-04-15 10:31:54 -0400

    Use camelCase in User Schemas

--- models/profile.js
+++ models/profile.js
@@ -3,21 +3,21 @@ const Schema = mongoose.Schema;
 
 // Define Profile model
 const ProfileSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   website: { type: String, unique: true },
   pronouns: { type: String },
   title: { type: String },
   bio: { type: String },
-  profile_photo_url: { type: String },
-  profile_public: { type: Boolean, default: false },
+  profilePhotoUrl: { type: String },
+  profilePublic: { type: Boolean, default: false },
   following: [
     { type: Schema.Types.ObjectId, ref: "profile" }
   ],
   followers: [
     { type: Schema.Types.ObjectId, ref: "profile" }
   ],
-  user_id: { type: Schema.Types.ObjectId, ref: "user", required: true, unique: true }
+  userId: { type: Schema.Types.ObjectId, ref: "user", required: true, unique: true }
 });
 
 // Export model
--- models/user.js
+++ models/user.js
@@ -6,11 +6,10 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
   email: { type: String, required: true, unique: true},
-  last_login: { type: Date, default: Date.now },
-  profile_id: { type: Schema.Types.ObjectId, ref: "profile", unique: true }
+  createdDate: { type: Date, default: Date.now },
+  lastLogin: { type: Date, default: Date.now },
+  profileId: { type: Schema.Types.ObjectId, ref: "profile", unique: true }
 });
 
 // Export model
