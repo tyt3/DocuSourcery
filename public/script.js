f77fa1cdb47a9f5a7c61e7c446e80cf9a7ec23a8
Tyrica Terry Kapral <tyt3@pitt.edu>
2024-04-15 20:42:43 -0400

    Update index and styling

--- public/script.js
+++ public/script.js
@@ -26,3 +26,37 @@ window.addEventListener('scroll', () => {
   }
 });
 
+// Calculate and set viewport height, minus top navbar
+
+// Get the viewport height
+const viewportHeight = window.innerHeight;
+
+// Get the top position of the div
+const divTop = document.querySelector('.vh-ds-100').getBoundingClientRect().top;
+
+// Calculate the height of the div
+const divHeight = viewportHeight - divTop;
+
+// Set the height of the div
+document.querySelector('.vh-ds-100').style.height = divHeight + 'px';
+
+// Add and Remove height for project navbar, depending on viewport
+window.addEventListener('resize', function() {
+  // Get the viewport width
+  const viewportWidth = window.innerWidth;
+
+  // Select the side-nav div
+  const sideNav = document.querySelector('.project-nav');
+
+  // Check if viewport width is less than 1200px
+  if (viewportWidth < 1200) {
+    // Remove the height property
+    sideNav.style.removeProperty('height');
+  } else {
+    // Add the height property back
+    sideNav.style.height = divHeight;
+  }
+});
+
+// Trigger the resize event on page load to initially set the height
+window.dispatchEvent(new Event('resize'));
