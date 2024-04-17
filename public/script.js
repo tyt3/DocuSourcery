706724d36d1b68e22e7a5ccf52b58e19e1b040ee
Tyrica Terry Kapral <tyt3@pitt.edu>
2024-04-17 13:55:19 -0400

    Update project sidenav styling

--- public/script.js
+++ public/script.js
@@ -75,7 +75,6 @@ var i;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
