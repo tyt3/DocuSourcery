242ffc73355ba68f411d8fa3221f3dd62e67d18a
Tyrica Terry Kapral <tyt3@pitt.edu>
2024-04-17 22:44:28 -0400

    Add slug field to project model

--- models/tag.js
+++ models/tag.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 // Define Tag model
 const TagSchema = new Schema({
   title: { type: String, maxLength: 50, unique: true, required: true },
+  slug: { type: String, required: true, maxLength: 50 },
   description: { type: String },
   projects: [
     { type: Schema.Types.ObjectId, ref: "project" }
