76ea46f4b8ceab7a379af44de0ff7c7aaab42ed8
Tyrica Terry Kapral <34420663+tyt3@users.noreply.github.com>
2024-04-25 14:18:43 -0400

    Test restore documents

--- routes/project.js
+++ routes/project.js
@@ -466,6 +466,43 @@ router.post('/project/remove-user/:id', ensureAuth, async (req, res) => {
   }
 });
 
+// Restore documents to project
+router.post('/project/restore-documents/:id', ensureAuth, async (req, res) => {
+  const projectId = req.params.id;
+  const { selectedDocuments } = req.body;
+
+  try {
+    // Find the project
+    const project = await Project.findById(projectId);
+    if (!project) {
+      return res.status(404).send("Project not found.");
+    }
+
+    const docsToRestore = Array.isArray(selectedDocuments) ? selectedDocuments : [selectedDocuments];
+
+    // Loop through the docIDs in docsToRestore, get the document from the Document model,
+    // set `deleted` to false, and save the document
+    for (const docId of docsToRestore) {
+      // Find the document by ID
+      const document = await Document.findById(docId);
+      if (document) {
+        // Set `deleted` to false
+        document.deleted = false;
+        // Save the updated document
+        await document.save();
+      }
+    }
+    
+    // Redirect to the project edit page
+    res.redirect(`/project/${project.slug}/edit`);
+    
+  } catch (err) {
+    console.error("Failed to restore document to the project:", err);
+    res.status(500).send("Server error occurred while trying to restore a document to the project.");
+  }
+});
+
+
 // Delete documents from project
 router.post('/project/delete-documents/:id', ensureAuth, async (req, res) => {
   const projectId = req.params.id;
